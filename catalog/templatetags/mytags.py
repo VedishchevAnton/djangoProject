60ae41cc276fec31597980fd31d01d296075ee14
Anton Vedischev <vedishchevanton@gmail.com>
2023-05-27 02:47:24 +0300

    попытка реализации тега

--- catalog/templatetags/mytags.py
+++ catalog/templatetags/mytags.py
@@ -1,10 +1,10 @@
 from django import template
 from django.conf import settings
+from urllib.parse import urljoin
 
 register = template.Library()
 
 
-@register.simple_tag
-def mediapath(image_path):
-    full_image_path = settings.MEDIA_URL + str(image_path)
-    return full_image_path
+@register.filter
+def media_url(value):
+    return urljoin(settings.MEDIA_URL, value)
