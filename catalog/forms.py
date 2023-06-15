ae37b7c6ad2b269cb9114ace4b92718fbe0c5f83
Anton Vedischev <vedishchevanton@gmail.com>
2023-06-15 18:52:45 +0300

    добавление комментариев в проверку с запрещенными словами

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,20 +1,21 @@
 from django import forms
-
-from catalog.models import Product
+from .models import Product
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ('name', 'description', 'category', 'price')
+        fields = ['name', 'description', 'price']
 
     def clean(self):
         cleaned_data = super().clean()
         name = cleaned_data.get('name')
         description = cleaned_data.get('description')
         forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
-                           'радар']
+                           'радар']  # список запрещенных слов
         for word in forbidden_words:
+            # Проверяем, содержит ли название или описание запрещенное слово
             if word in name.lower() or word in description.lower():
+                # Если содержит, генерируем исключение с сообщением об ошибке
                 raise forms.ValidationError(f'Слово "{word}" запрещено в названии и описании продукта.')
         return cleaned_data
