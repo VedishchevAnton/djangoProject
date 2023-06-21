9016347e8b3e636384ca498318dfde17ef3d62b0
Anton Vedischev <vedishchevanton@gmail.com>
2023-06-21 18:54:45 +0300

    удаление ошибок

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetDoneView, PasswordResetConfirmView, \
-    PasswordResetCompleteView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView, MyPasswordResetView
+from users.views import RegisterView, ProfileView
 
 app_name = UsersConfig.name
 
@@ -12,10 +11,4 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    # path('verify-email/<int:pk>/', VerifyEmailView.as_view(), name='verify_email'),
-    # path('password-reset/', MyPasswordResetView.as_view(), name='password_reset'),
-    # path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    # path('password-reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('password-reset/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.views import PasswordResetView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
@@ -22,31 +21,3 @@ class ProfileView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
-
-    # def form_valid(self, form):
-    #     response = super().form_valid(form)
-    #     subject = 'Подтвердите вашу почту'
-    #     message = 'Пожалуйста, нажмите на ссылку ниже, чтобы подтвердить свой адрес электронной почты:\n\n{0}'.format(
-    #         self.request.build_absolute_uri(reverse_lazy('users:verify-email', kwargs={'pk': self.object.pk}))
-    #     )
-    #     from_email = 'noreply@example.com'
-    #     recipient_list = [self.object.email]
-    #     send_mail(subject, message, from_email, recipient_list, fail_silently=False)
-    #     return response
-
-
-#
-#
-# class VerifyEmailView(View):
-#     def get(self, request, pk):
-#         user = User.objects.get(pk=pk)
-#         user.email_verified = True
-#         user.save()
-#         return redirect('users:profile')
-
-# class MyPasswordResetView(PasswordResetView):
-#     email_template_name = 'users/password_reset_email.html'  # имя шаблона для отправки письма с инструкциями по
-#     # восстановлению пароля
-#     success_url = reverse_lazy(
-#         'password_reset_done')  # URL, на который будет перенаправлен пользователь после успешной отправки письма
-#     template_name = 'users/password_reset_form.html'  # имя шаблона для отображения формы восстановления пароля
