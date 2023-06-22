0a17a0e064e58681f671fdc27c298c7154fae0bd
Anton Vedischev <vedishchevanton@gmail.com>
2023-06-22 19:16:59 +0300

    верификация почты пользователя через отправленное письмо

--- users/models.py
+++ users/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
     country = models.CharField(max_length=100, verbose_name='Страна', **NULLABLE)
 
-    email_verify = models.BooleanField(default=False, verbose_name='Проверка адреса почты')
+    is_active = models.BooleanField(default=False, verbose_name='Подтверждение почты')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
